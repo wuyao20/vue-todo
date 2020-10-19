f74f8ff517de63fb969e03518a4ba47ad5b9c422
wuyao20 <huyashangding@163.com>
2020-10-19 15:38:15 +0800

    init project

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id='text'>
+    {{text}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: 'salted fish'
+      }
+    },
+  }
+</script>
+
+<style scoped>
+ #text{
+   color:red;
+ }
+</style>
