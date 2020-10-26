6c96a2ff682a9c52021652f9bc614cbb04b4e476
wuyao20 <huyashangding@163.com>
2020-10-26 09:17:22 +0800

    mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isDev=process.env.NODE_ENV === 'development'
 
 const config = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
   },
   module: {
@@ -21,27 +21,6 @@ const config = {
       {
         test: /\.jsx$/,
         loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -70,6 +49,21 @@ const config = {
 }
 
 if(isDev) {
+  config.output.filename = 'bundle.[hash:8].js',
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     contentBase: path.join(__dirname, "dist"),
@@ -77,6 +71,23 @@ if(isDev) {
     hot: true
   }
   config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}else {
+  config.output.filename = 'bundle.[chunkhash:8].js',
+  config.module.rules.push({
+    test: /\.styl/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
+  config.plugins.push(new MiniCssExtractPlugin())
 }
 
 module.exports = config
